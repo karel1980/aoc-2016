971e8a5225fb1e8dec7caabe540cd7ed66b08ea2
Karel Vervaeke <karel.vervaeke@aca-it.be>
2016-12-25 06:58:01 +0100

    Day25

---
+++ day25/day25.py
@@ -0,0 +1,7 @@
+
+print "after careful inspection of the assembunny code, the answer must be"
+print "the first number with binary pattern 1010101010... greater than 365*7"
+
+print "I ended up just doing bin(365*7), and manually typed 0b101010101... until I had a value greater than 365*7,"
+print "and then did 365*7 - 0b1010101... - that was the correct answer. Yay. High fives all around"
+
